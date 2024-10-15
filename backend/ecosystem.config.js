f03cf364dfed6af3a36dbc196620df500c2c4910
Stepan Sukhachev <droiderlinux@yahoo.com>
2024-10-15 08:28:43 +0300

    chore: add ecosystem.config.js for PM2 deployment

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  JWT_SECRET,
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      script: './dist/app.js',
+      env_production: {
+        NODE_ENV: 'production',
+        JWT_SECRET,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/Estisui/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      key: 'vm-6',
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
