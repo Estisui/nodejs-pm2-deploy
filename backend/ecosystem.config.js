e714b9ed62d9aa519092454b4987c2b3d0c41827
Stepan Sukhachev <droiderlinux@yahoo.com>
2024-12-02 22:52:07 +0300

    refactor: get rid of excessive info in .env + move all enviromental vars to single file

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-require('dotenv').config({ path: '.env.deploy' });
 
 const {
   JWT_SECRET,
@@ -7,6 +6,7 @@ const {
   DEPLOY_HOST,
   DEPLOY_PATH,
   DEPLOY_REF = 'origin/master',
+  DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
@@ -26,7 +26,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/Estisui/nodejs-pm2-deploy.git',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
       'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -5,6 +5,7 @@ const {
   DEPLOY_HOST,
   DEPLOY_PATH,
   DEPLOY_REF = 'origin/master',
+  DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
@@ -13,7 +14,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/Estisui/nodejs-pm2-deploy.git',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
     },
